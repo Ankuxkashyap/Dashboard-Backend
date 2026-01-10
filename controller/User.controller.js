e62ad737ffb017da18e3e7b9962a4fc304936feb
Ankuxkashyap <ankitdochak0001@gmail.com>
2026-01-11 03:28:05 +0530

    fix: add sameSite attribute to cookies in register and login functions

--- controller/User.controller.js
+++ controller/User.controller.js
@@ -34,6 +34,7 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, 
+      sameSite: "lax",
       path: "/",
     });
 
@@ -75,6 +76,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
+      sameSite: "lax",
       path: "/",
     });
     res.status(200).json({ message: "Login Successful" ,user: { id: user._id, name: user.name, email: user.email }});
