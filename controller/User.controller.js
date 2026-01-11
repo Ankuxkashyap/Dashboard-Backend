d0ec411fa9654d83895719df02a995df52f33418
Ankuxkashyap <ankitdochak0001@gmail.com>
2026-01-11 15:28:57 +0530

    fix: remove sameSite attribute from cookies in register and login functions

--- controller/User.controller.js
+++ controller/User.controller.js
@@ -34,7 +34,6 @@ export const register = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, 
-      sameSite: "lax",
       path: "/",
     });
 
@@ -76,7 +75,6 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-      sameSite: "lax",
       path: "/",
     });
     res.status(200).json({ message: "Login Successful" ,user: { id: user._id, name: user.name, email: user.email }});
